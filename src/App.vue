44375cb9e6c5ef47e894029c3f87b6af35a347ac
Artem Dragunov <dragunovartem99@gmail.com>
2022-08-23 20:38:07 +0300

    Add tag component and basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,35 @@
-<script setup></script>
+<template>
+  <h1>O&#8322; Reporter</h1>
+  <div class="app">
+    <textarea v-model="test" name="" id="" cols="30" rows="10"></textarea>
+    <div class="code">
+      <fakeTag type="p">{{ test }}</fakeTag>
+    </div>
+  </div>
+</template>
 
-<template></template>
+<script>
+import fakeTag from "./components/fakeTag.vue";
 
-<style scoped></style>
+export default {
+  components: {
+    fakeTag,
+  },
+  data() {
+    return {
+      test: "",
+    };
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  text-align: center;
+}
+.app {
+  display: flex;
+}
+.code {
+}
+</style>
---
+++ src/components/fakeTag.vue
@@ -0,0 +1,8 @@
+<template>
+  <span> &lt;{{ type }}&gt;<slot />&lt;/{{ type }}&gt; </span>
+</template>
+<script>
+export default {
+  props: ["type"],
+};
+</script>
